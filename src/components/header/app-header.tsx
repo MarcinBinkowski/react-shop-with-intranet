69daa258ced9d7abf3c0adc9de152e2695d162f3
Marcin <binq661@gmail.com>
2025-04-01 13:24:53 +0200

    test

--- src/components/header/app-header.tsx
+++ src/components/header/app-header.tsx
@@ -1,21 +1,5 @@
 "use client"
-
-import { Bell, Search, Menu, MessageSquare, LogIn } from "lucide-react"
-import { Button } from "@/components/ui/button"
-import { Input } from "@/components/ui/input"
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 import { ModeToggle } from "@/components/mode-toggle"
-// import { ThemeToggle } from "@/components/theme-toggle"
-import { useUser } from "@/context/user-context"
-import { useNavigate } from "react-router-dom"
 import {ShowMenuButton} from "./show-menu-button"
 import { AppHeaderProps } from "./types"
 import {AuthActions} from "./auth-actions"
@@ -31,7 +15,8 @@ export function AppHeader({
 
 
   return (
-    <header className="sticky top-0 z-30 flex h-16 w-full items-center justify-between border-b bg-background/95 px-4 backdrop-blur supports-[backdrop-filter]:bg-background/60 lg:px-6">
+    <>
+      <header className="sticky top-0 z-30 flex h-16 w-full items-center justify-between border-b bg-background/95 px-4">
       <div className="flex items-center gap-4">
         <ShowMenuButton onMenuClick={onMenuClick} />
         <h1 className="text-xl font-semibold">{title}</h1>
@@ -45,6 +30,8 @@ export function AppHeader({
         />
       </div>
     </header>
+    </>
+
   )
 }
 
