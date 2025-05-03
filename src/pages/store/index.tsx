bc1070bb664667f3d5f0f82942558a0d909c4b96
Marcin <binq661@gmail.com>
2025-05-03 19:34:56 +0200

    test

--- src/pages/store/index.tsx
+++ src/pages/store/index.tsx
@@ -4,7 +4,7 @@ import { BarChart3, Package, ShoppingCart, Users } from "lucide-react"
 export default function Index() {
   return (
     <div className="space-y-6 max-w-7xl mx-auto">
-      <h1>hello world</h1>
+      <h1>Marcin Binkowski, shop + intranet, react + .net webapi</h1>
     </div>
   )
 }
